f0c7fab001c32d0fc89d6e585499dde151a935ce
Mahlatse <morwawarabothata5@gmail.com>
2024-04-29 12:01:13 +0200

    Made a signup page

--- app/src/main/java/com/example/parkingapp/signup.java
+++ app/src/main/java/com/example/parkingapp/signup.java
@@ -1,9 +1,6 @@
 package com.example.parkingapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class getStarted extends AppCompatActivity {
-    Button signUpPage;
+public class signup extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_get_started);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-        signUpPage = findViewById(R.id.signUpPage);
-
-        signUpPage.setOnClickListener(v -> openSignUpPage(v));
-    }
-
-    public void openSignUpPage(View view) {
-        Intent intent = new Intent(this, signup.class);
-        startActivity(intent);
     }
 }
