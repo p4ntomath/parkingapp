86936d06c3bf45f7ec7caf5bca6ba40551b13b49
Mahlatse <morwawarabothata5@gmail.com>
2024-05-01 22:12:31 +0200

    Added Menu Button functionality

--- app/src/main/java/com/example/parkingapp/navigationDrawer.java
+++ app/src/main/java/com/example/parkingapp/navigationDrawer.java
@@ -1,15 +1,20 @@
 package com.example.parkingapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class navigationDrawer extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    ImageButton menuBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,14 @@ public class navigationDrawer extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        drawerLayout = findViewById(R.id.drawer);
+        menuBtn = findViewById(R.id.menuBtn);
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
         });
     }
 }
