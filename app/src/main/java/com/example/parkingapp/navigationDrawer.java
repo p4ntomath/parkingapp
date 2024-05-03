a1170c897f15e785c7fd73f7e3ad7da168cdefd0
Mahlatse <morwawarabothata5@gmail.com>
2024-05-03 22:15:05 +0200

    Initial Commit

--- app/src/main/java/com/example/parkingapp/navigationDrawer.java
+++ app/src/main/java/com/example/parkingapp/navigationDrawer.java
@@ -1,20 +1,29 @@
 package com.example.parkingapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class navigationDrawer extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
+    NavigationView navigationView;
     ImageButton menuBtn;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +36,48 @@ public class navigationDrawer extends AppCompatActivity {
         });
         drawerLayout = findViewById(R.id.drawer);
         menuBtn = findViewById(R.id.menuBtn);
+        navigationView = findViewById(R.id.navigationView);
+        frameLayout = findViewById(R.id.fragmentLayout);
+
+
         menuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drawerLayout.open();
             }
         });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                menuItem.setChecked(true);
+                drawerLayout.close();
+                if (id == R.id.nav_home) {
+                    replaceFragment(new home_fragment());
+                    return true;
+                }else if (id == R.id.nav_booking) {
+                    // Handle profile item click
+                    return true;
+                }else if (id == R.id.nav_reserve) {
+                    // Handle settings item click
+                    return true;
+                }else if (id == R.id.nav_help) {
+                    // Handle settings item click
+                    return true;
+                }else if (id == R.id.nav_about) {
+                    // Handle settings item click
+                    return true;
+                }else if(id == R.id.nav_logout){
+                    // Handle settings item click
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragmentLayout,fragment).commit();
     }
 }
