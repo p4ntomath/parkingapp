407cd9b3bce69b57108a6faf444ce22756cf2367
Mahlatse <morwawarabothata5@gmail.com>
2024-05-09 01:34:38 +0200

    Map View might be working

--- app/src/main/java/com/example/parkingapp/home_fragment.java
+++ app/src/main/java/com/example/parkingapp/home_fragment.java
@@ -1,64 +1,62 @@
 package com.example.parkingapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link home_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class home_fragment extends Fragment {
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class home_fragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     public home_fragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment home_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static home_fragment newInstance(String param1, String param2) {
-        home_fragment fragment = new home_fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.home_fragment, container, false);
+
+        // Initialize the SupportMapFragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+            getChildFragmentManager().beginTransaction()
+                    .replace(R.id.map, mapFragment)
+                    .commit();
+        }
+
+        mapFragment.getMapAsync(this);
+
+        return rootView;
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng johannesburg = new LatLng(-26.2044, 28.0456);
+        mMap.addMarker(new MarkerOptions().position(johannesburg).title("Marker in Johannesburg"));
+
+        // Move camera to Johannesburg and zoom closer
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(johannesburg, 15));
+
+
+        mMap.setMaxZoomPreference(15); // Set max zoom level
+
+    }
+
 }
