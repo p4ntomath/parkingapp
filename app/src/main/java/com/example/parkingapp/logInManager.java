99296583ef45959d444274202060c6bacda11bdf
Mahlatse <morwawarabothata5@gmail.com>
2024-05-13 13:24:16 +0200

    converted signup and log in pages as bottomsheet diaolog instead of activities

---
+++ app/src/main/java/com/example/parkingapp/logInManager.java
@@ -0,0 +1,145 @@
+package com.example.parkingapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class logInManager {
+
+    private Context context;
+    private EditText logInUserId;
+    private EditText logInPassword;
+    public boolean openNav = false;
+
+    public logInManager(Context context, EditText logInUserId, EditText logInPassword) {
+        this.context = context;
+        this.logInUserId = logInUserId;
+        this.logInPassword = logInPassword;
+    }
+
+    public void logIn() {
+        String userIdString = logInUserId.getText().toString();
+        String password = logInPassword.getText().toString();
+
+
+        if (validateForm(userIdString, password)) {
+            SQLReq(userIdString, password);
+            logInUserId.setText("");
+            logInPassword.setText("");
+        }
+    }
+
+    private boolean validateForm(String userIdString, String password) {
+        if (userIdString.isEmpty()) {
+            logInUserId.setError("Person Number is required");
+            return false;
+        } else if (!validateUserId(userIdString)) {
+            logInUserId.setError("Please enter a valid Person Number");
+            logInUserId.setText("");
+            return false;
+        } else if (password.isEmpty()) {
+            logInPassword.setError("Password is required");
+            return false;
+        } else if (!validatePassword(password)) {
+            logInPassword.setError("Password must be at least 8 characters");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateUserId(String userId) {
+        return userId.length() == 7;
+    }
+
+    private boolean validatePassword(String password) {
+        return password.length() >= 8;
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(context.getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private void showToastOnUiThread(String message) {
+        ((Activity) context).runOnUiThread(new Runnable() {@Override
+            public void run() {
+                showToast(message);
+            }
+        });
+    }
+
+    private void storeToSharedPreferences(String userIdString, String email, String password, String uType) {
+        userSessionManager sessionManager = new userSessionManager(context);
+        sessionManager.createSession(userIdString, uType, email, password);
+    }
+
+    private void SQLReq(String userIdString, String password) {
+        OkHttpClient client = new OkHttpClient();
+
+        String parseUrl = "https://lamp.ms.wits.ac.za/home/s2691450/login.php";
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(parseUrl).newBuilder();
+        urlBuilder.addQueryParameter("userIdString", userIdString);
+        urlBuilder.addQueryParameter("password", password);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                showToast("Failed to connect to server");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    showToast("Failed to connect to server");
+                    return;
+                }
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    String outcome = jsonObject.getString("OUTCOME");
+
+                    if (outcome.equals("exists")) {
+                        showToastOnUiThread("Login Successful");
+                        String email = jsonObject.getString("EMAIL");
+                        String uType = jsonObject.getString("USER_TYPE");
+                        String userId = jsonObject.getString("USER_ID");
+                        String password = jsonObject.getString("PASSWORD");
+
+                        storeToSharedPreferences(userId, email, password, uType);
+                        openNavigationDrawer();
+                    } else {
+                        showToastOnUiThread("User does not exist");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+    }
+
+    public void openNavigationDrawer(){
+        Intent intent = new Intent(context, navigationDrawer.class);
+        context.startActivity(intent);
+        ((Activity) context).finish();
+    }
+
+}
---
+++ app/src/main/java/com/example/parkingapp/signUpManager.java
@@ -0,0 +1,159 @@
+package com.example.parkingapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class signUpManager {
+
+    private Context context;
+    private EditText signUpuserId;
+    private EditText signUpEmail;
+    private EditText signUpPassword;
+    private RadioGroup userType;
+    private TextView errorMessage;
+
+    public signUpManager(Context context, EditText signUpuserId, EditText signUpEmail,
+                         EditText signUpPassword, RadioGroup userType, TextView errorMessage) {
+        this.context = context;
+        this.signUpuserId = signUpuserId;
+        this.signUpEmail = signUpEmail;
+        this.signUpPassword = signUpPassword;
+        this.userType = userType;
+        this.errorMessage = errorMessage;
+    }
+
+    public void signUp() {
+        String userIdString = signUpuserId.getText().toString();
+        String email = signUpEmail.getText().toString();
+        String password = signUpPassword.getText().toString();
+        int selectedId = userType.getCheckedRadioButtonId();
+        String uType = (selectedId == R.id.student) ? "Student" : "Staff";
+
+        if (validateForm(userIdString, email, password, selectedId)) {
+            SQLReq(userIdString, email, password, uType);
+        }
+    }
+
+    private boolean validateUserId(String userId) {
+        return userId.length() == 7;
+    }
+
+    private boolean validatePassword(String password) {
+        return password.length() >= 8;
+    }
+
+    private boolean validateEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean validateUserType(int userTypeId) {
+        return userTypeId != -1;
+    }
+
+    private boolean validateForm(String userIdString, String email, String password, int userTypeId) {
+        if (userIdString.isEmpty()) {
+            signUpuserId.setError("Please enter your Person Number");
+            return false;
+        } else if (!validateUserId(userIdString)) {
+            signUpuserId.setError("Please enter a valid Person Number");
+            signUpuserId.setText("");
+            return false;
+        } else if (email.isEmpty()) {
+            signUpEmail.setError("Please enter your email");
+            return false;
+        } else if (!validateEmail(email)) {
+            signUpEmail.setError("Please enter a valid email");
+            signUpEmail.setText("");
+            return false;
+        } else if (!validatePassword(password)) {
+            signUpPassword.setError("Password must be at least 8 characters");
+            signUpPassword.setText("");
+            return false;
+        } else if (!validateUserType(userTypeId)) {
+            errorMessage.setVisibility(View.VISIBLE);
+            errorMessage.setText("Please select a user type");
+            return false;
+        }
+        return true;
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(context.getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private void showToastOnUiThread(String message) {
+        ((Activity) context).runOnUiThread(new Runnable() {@Override
+        public void run() {
+            showToast(message);
+        }
+        });
+    }
+
+    private void storeToSharedPreferences(String userIdString, String email, String password, String uType) {
+        userSessionManager sessionManager = new userSessionManager(context);
+        sessionManager.createSession(userIdString, uType, email, password);
+    }
+
+    private void openNavigationDrawer() {
+        Intent intent = new Intent(context, navigationDrawer.class);
+        context.startActivity(intent);
+        ((Activity) context).finish();
+    }
+
+    private void SQLReq(String userIdString, String email, String password, String uType) {
+        String url = "https://lamp.ms.wits.ac.za/home/s2691450/signup.php";
+
+        AsyncTask.execute(() -> {
+            OkHttpClient client = new OkHttpClient();
+
+            RequestBody requestBody = new FormBody.Builder()
+                    .add("userIdString", userIdString)
+                    .add("password", password)
+                    .add("userType", uType)
+                    .add("email", email)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(requestBody)
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+
+                if (response.isSuccessful()) {
+                    String responseBody = response.body().string();
+                    if (responseBody.equals("success")) {
+                        showToastOnUiThread("Account created successfully");
+                        storeToSharedPreferences(userIdString, email, password, uType);
+                        openNavigationDrawer();
+                    } else if (responseBody.equals("failed")) {
+                        showToastOnUiThread("Failed to create an account");
+                    } else if (responseBody.equals("exists")) {
+                        showToastOnUiThread("Account already exists. Sign in");
+                    }
+                } else {
+                    showToastOnUiThread("Failed to create an account");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
