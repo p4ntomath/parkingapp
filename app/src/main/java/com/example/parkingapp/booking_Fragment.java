aae164d5b334f7f3a45e662254ee4cef627ac96d
Mahlatse <morwawarabothata5@gmail.com>
2024-05-14 20:23:18 +0200

    no sure about layouts i need android phone

--- app/src/main/java/com/example/parkingapp/booking_Fragment.java
+++ app/src/main/java/com/example/parkingapp/booking_Fragment.java
@@ -19,42 +19,16 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link booking_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class booking_Fragment extends Fragment implements selectListner {
 
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public booking_Fragment() {
-        // Required empty public constructor
-    }
 
 
-    public static booking_Fragment newInstance(String param1, String param2) {
-        booking_Fragment fragment = new booking_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+
+
 
     parkingSlotItem item = new parkingSlotItem(R.drawable.cartopviewleft,R.drawable.cartopviewright);
     parkingSlotAdapter adapter;
