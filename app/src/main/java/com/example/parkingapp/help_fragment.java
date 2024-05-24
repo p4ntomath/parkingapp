f8da666692f7cbc73288802aac8942a2bad5df65
Mahlatse <morwawarabothata5@gmail.com>
2024-05-24 14:15:37 +0200

    card profilr done

--- app/src/main/java/com/example/parkingapp/help_fragment.java
+++ app/src/main/java/com/example/parkingapp/help_fragment.java
@@ -3,62 +3,62 @@ package com.example.parkingapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link help_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class help_fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+        View viewParam;
     public help_fragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment help_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static help_fragment newInstance(String param1, String param2) {
-        help_fragment fragment = new help_fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public help_fragment(View view) {
+        this.viewParam = view;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    RelativeLayout howToUse,faq,support;
+    TextView howToUseText,faqText,supportText;
 
+View supportDevsView;
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.help_fragment, container, false);
+        view = inflater.inflate(R.layout.help_fragment, container, false);
+        howToUse = view.findViewById(R.id.howToUse);
+        faq = view.findViewById(R.id.faqs);
+        support = view.findViewById(R.id.supportDevs);
+        howToUseText = view.findViewById(R.id.howToUseText);
+        faqText = view.findViewById(R.id.faqsText);
+        supportText = view.findViewById(R.id.supportDevsText);
+
+        support.setOnClickListener(v->{
+            supportDevsView = inflater.inflate(R.layout.supportdevelopers, container, false);
+            Fragment about = new help_fragment(supportDevsView);
+            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragmentLayout,about);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        });
+        if(viewParam!=null){
+            view = viewParam;
+
+        }
+
+        return view;
     }
+
+
+
+
+
+
 }
